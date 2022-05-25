3cad75af1e16ebe1906931bdee4bd4e4727e0ddd
dinesh <dinesh.n@klinikeverywhere.com>
2022-05-25 13:03:14 +0530

    make it better

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 module.exports = async (sql, args) => {
-  const  connect = {
+  const  connect = { 
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
